8d01fccd0561f7a153f710cb194f311a30db059a
Ygin White <yginwhite@gmail.com>
2016-01-21 13:17:46 +0200

    add functy to view & add constructor Card to model

--- js/index.js
+++ js/index.js
@@ -1,21 +1,28 @@
 (function () {
     'use strict';
-    var model, view, controller;
+    var model, view, controller, global;
 
-    model = {
+    global = {
+        start: document.getElementById('start'),
+        again: document.getElementById('again'),
+        view: document.getElementById('vw'),
+        tip: document.getElementById('tips'),
         image: (function () {
             var img = new Image();
             img.src = 'img/suite.png';
             return img;
-        }()),
+        }())
+    };
+
+    model = {
         randomNumber: function (x, y) {
             y = y - 1;
             return Math.floor(Math.random() * y + x);
         },
-        makeSuite: function (width) {
+        makeSuite: function () {
             var suite = [], n;
             while (true) {
-                n = this.randomNumber(0, 35) * width;
+                n = this.randomNumber(0, 35);
                 if (suite.indexOf(n) === -1) {
                     suite.push(n);
                 }
@@ -24,26 +31,52 @@
                 }
             }
             return suite;
+        },
+        Card: function (n, width) {
+            this.id = n * width;
+            this.y = this.id + 2;
+            this.x = this.y - this.id - 1;
+        }
+    };
+    model.Card.prototype.make = function() {
+        var img = global.image.cloneNode();
+        var cont = document.createElement('div');
+        cont.style.width = '101px';
+        cont.style.height = '150px';
+        //cont.style.background = '#D3CFCF url("../img/suite.png") no-repeat';
+        cont.classList.add('img');
+        return cont;
+    };
+
+
+    view = {
+        cardSet: function () {
+            var suite = [];
+            var set = model.makeSuite();
+            var i;
+            for (i = 0; i < set.length; i += 1) {
+                suite.push(new model.Card(set[i], 100));
+            }
+            return suite;
+        },
+        showCards: function () {
+            var set = this.cardSet();
+            var card = set[1].make();
+            global.view.querySelector('.output').appendChild(card);
         }
     };
 
-    view = {
-
-    };
-
     controller = {
-        start: document.getElementById('start'),
-        again: document.getElementById('again'),
-        vw: document.getElementById('vw'),
         init: function () {
-            var self = this;
-            this.start.onclick = function () {
-                self.vw.removeChild(wellcome);
+            global.start.onclick = function () {
+                global.wellcome = global.tip.removeChild(wellcome);
                 /*self.vw.appendChild(model.image.cloneNode(true));
                 self.vw.appendChild(model.image.cloneNode(true));*/
+                console.log(global);
             };
         }
     };
 
     controller.init();
+    console.log(view.showCards());
 }());
